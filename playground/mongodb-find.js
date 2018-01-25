3a9f02a58ca0493446ce7c4dce66315ffb521d25
unknown <tpridge@clemson.edu>
2018-01-25 16:40:38 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID("5a6a447758fe6ecb0da366a5")}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // });
+
+    db.collection('Users').find({
+        name: "Brad"}).toArray().then((users) => {
+        console.log('Users');
+        console.log(JSON.stringify(users, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err)
+    });
+
+    db.close();
+});
