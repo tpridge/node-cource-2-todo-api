d4dccc261219537bcf03f86104f8595e3875e579
unknown <tpridge@clemson.edu>
2018-03-04 21:08:19 -0500

    add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,35 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		resp.status(400).send(e);
 	});
+});
+
+
+// GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	// valdate id using isValid
+		// 404 - send back empty body
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	} else {
+		Todo.findById(id).then((todo) => {
+			if (!todo) {
+				res.status(404).send();
+			} else {
+				res.send({todo});
+			}
+		}).catch((e) => {
+			res.status(400).send();
+		});
+	}
+
+	// findById
+		// success 
+			// if todo - send it back
+			// if no todo - send back 404 with empty body
+		// error
+			// 400 - and send empty body back
 });
 
 app.listen(3000, () => {
