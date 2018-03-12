8d6a772c5f6f5339c348862fd1a9e4863f6a3cb4
Brad Pridgeon <tpridge@clemson.edu>
2018-03-11 21:45:43 -0400

    Add tet cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const request = require('supertest');
 const expect = require('expect');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'First test todo'
 }, {
-	text: 'Second text todo'
+	_id: new ObjectID(),
+	text: 'Second test todo'
 }];
 
 beforeEach((done) => {
@@ -69,4 +72,36 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
+});
+
+
+describe('GET /todos/:id', () => {
+	it('should return todo doc', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text)
+			})
+			.end(done);
+	});
+
+
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectID().toHexString();
+
+		request(app)
+			.get(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+
+	});
+
+	it('should return 404 for non-object ids', (done) => {
+		// /todos/123
+		request(app)
+			.get('/tdods/123abc')
+			.expect(404)
+			.end(done);
+	});
 });
