7597c5d7c273fd43c8d1dd3d10c20cca1a61a245
Brad Pridgeon <tpridge@clemson.edu>
2018-05-09 21:38:58 -0400

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -138,7 +138,6 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
-// POST /users/login (email, password)
 app.post('/users/login', (req, res) => {
 	var body = _.pick(req.body, ['email', 'password']);
 	
